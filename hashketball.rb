a72ff7404c0f913d7d873f765df08a665e5e3ed7
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 20:20:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -48,7 +48,8 @@ def game_hash
         steals: 7,
         blocks: 5,
         slam_dunks: 12 }
-    ] },
+    ]
+   },
   home: { team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
     players: [
@@ -97,6 +98,7 @@ def game_hash
       steals: 4,
       blocks: 11,
       slam_dunks: 1 }
-    ] }
+    ]
+  }
 }
 end
