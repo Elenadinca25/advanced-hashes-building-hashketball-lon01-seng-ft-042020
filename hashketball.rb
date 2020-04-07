7339b54c4c0c295d4b6a1d075287833124ddc648
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 19:58:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -50,8 +50,7 @@ def game_hash {
         slam_dunks: 12 }
     ]
   }
-  home: {
-    team_name: "Brooklyn Nets",
+  home: { team_name: "Brooklyn Nets",
     colors: %w[ Black White],
     players: [
   { player_name: "Alan Anderson",
